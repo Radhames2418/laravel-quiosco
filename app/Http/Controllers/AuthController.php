d41eea97b473d29ff1b37203620e11016b483a99
Radhames2418 <Radhamesenc2412002@gmail.com>
2023-08-10 17:09:24 -0400

    finished user module authentication

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegistroRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -26,11 +28,34 @@ class AuthController extends Controller
         ];
     }
 
-    public function login(Request $request){
+    public function login(LoginRequest $request)
+    {
+        $data = $request->validated();
+
+        if (!Auth::attempt($data))
+        {
+            return response([
+               'errors' => [' El email o el password son incorrecto ']
+            ], 422);
+        }
+
+        // Autenticar al usuario
+
+        $user = Auth::user();
+
+        return [
+            'token' => $user->createToken('token')->plainTextToken,
+            'user'  => $user
+        ];
 
     }
 
     public function logout(Request $request){
+        $user = $request->user();
+        $user->currentAccessToken()->delete();
 
+        return [
+            'user' => null
+        ];
     }
 }
